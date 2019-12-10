51cc6efd6059f0cd0088462f7df1cfffeb66edbc
Andreas Kull <akullpp@gmail.com>
2019-12-10 13:15:10 +0100

    Init

---
+++ generators/app/src/test/java/de/akull/generator/GeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.akull.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
